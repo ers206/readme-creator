e4628d7fdb146c893f32b23a3646bc1974cd8492
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-07-25 09:04:03 -0500

    alot to do still

--- Develop/index.js
+++ Develop/index.js
@@ -2,51 +2,15 @@
 const fs =require('fs');
 const inquirer = require('inquirer');
 
-var message = 'Hello Node!';
+// var message = 'Hello Node!';
 
-var sum = 5 + 3;
+// var sum = 5 + 3;
 
-console.log(message);
-console.log(sum);
+// console.log(message);
+// console.log(sum);
 
 // TODO: Create an array of questions for user input
-// const questions = [
-//     {
-//         type: 'input',
-//         name: 'creator',
-//         message: 'Who created this project?'
-//     },
-//     {
-//         type: 'input',
-//         name: 'email',
-//         message: 'What is your email address?'
-//     },
-//     {
-//         type: 'input',
-//         name: 'username',
-//         message: 'What is your Github username'
-//     },
-//     {
-//         type: 'input',
-//         name: 'project',
-//         message: 'What is the projects name?'
-//     },
-//     {
-//         type: 
-//         name:
-//         message:
-//     },
-//     {
-//         type: 
-//         name:
-//         message:
-//     },
-//     {
-//         type: 
-//         name:
-//         message:
-//     },
-// ]; 
+
 
 inquirer
 .prompt([
@@ -96,35 +60,32 @@ inquirer
     name: 'test',
     message: 'Please enter the test instructions'
 },
-{
-    type: 'list',
-    name: 'license',
-    message: 'Choose a license for your application',
-    choices: ['','','']
+// {
+//     type: 'list',
+//     name: 'license',
+//     message: 'Choose a license for your application',
+//     choices: ['','','']
 
-},
-{
-    type: '',
-    name: '',
-    message: ''
-},
-// WHEN I enter my project title 
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests 
-// WHEN I choose a license for my application from a list of options 
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// },
+// {
+//     type: '',
+//     name: '',
+//     message: ''
+// },
+
 ])
-.then(data => {
-    const filename = `${data.name
-      .toLowerCase()
-      .split('')
-      .join('')}.json`;
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), err=>
-      err ? console.log(err)  : console.log('it works!')
-    );
-})
+
+// .then(data => {
+//     const filename = `${data.name
+//       .toLowerCase()
+//       .split('')
+//       .join('')}.json`;
+
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), err=>
+//       err ? console.log(err)  : console.log('it works!')
+//     );
+// })
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
